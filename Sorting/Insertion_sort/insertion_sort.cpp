2149a1185854a578f53246abc63bc9d54b4b37b0
Mihir Mahajani <66313569+Mihir2527@users.noreply.github.com>
2020-10-13 09:07:14 +0530

    Created insertion_sort.cpp
    This is an in-place comparison-based sorting algorithm.
    
    Time Complexities:
    Best Case - O(n)
    Worst Case - O(n^2)
    Average Case - O(n^2)

---
+++ Sorting/Insertion_sort/insertion_sort.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+    int arr[n];
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    insertionSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}
